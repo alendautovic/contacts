e3dbdd3af102258d7193735cea083f2db90e5d70
Alen Dautović <alen.dautovic@amplitudo.me>
2020-10-17 16:29:45 +0200

    Spring project initialized

---
+++ src/test/kotlin/com/github/alendautovic/contacts/ContactsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.alendautovic.contacts
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ContactsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
